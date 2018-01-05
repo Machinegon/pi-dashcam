fde3066a6209939f26a9cb0253b7bb38821ceba1
machinegon <blind99@live.ca>
2018-01-04 20:33:38 -0500

    Revert back

--- src/etc/dashcam/dashcam-ctrl.sh
+++ src/etc/dashcam/dashcam-ctrl.sh
@@ -11,9 +11,13 @@
 # Set stop-detection gpio
 # The pi will stop recording when low and save the video
 SCRIPT=/etc/init.d/pi-dashcam.sh
+GPIO=/sys/class/gpio/
+POWERGPIO=11
 PID_FILE="/tmp/dashcam.pid"
 
 UPTIME=0
+cd $GPIO
+echo $POWERGPIO > export
 while true; do
     if [ -f $PID_FILE ]; then
         if [ $UPTIME -gt 900 ]; then # 15 minutes file rotation
@@ -21,6 +25,14 @@ while true; do
             $SCRIPT restart
         fi
         UPTIME=$((UPTIME+5))
+
+        # Power is down, stop and shutdown
+        IOVAL=`cat ${GPIO}gpio${POWERGPIO}/value`
+        if [ $IOVAL -eq 1 ]; then
+            echo "Power is down, stopping"
+            $SCRIPT stop
+            shutdown now
+        fi    
     else
         echo "Waiting for $SCRIPT PID ..."
     fi
